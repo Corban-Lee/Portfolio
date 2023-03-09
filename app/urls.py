56dac7e190444f023d6d29bc4dad1f621e9c6e11
Corban-Lee <77944149+XordK@users.noreply.github.com>
2023-03-09 13:35:40 +0000

    created landing page

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+"""URLs for the app."""
+
+from django.urls import path
+from .views import IndexView
+
+app_name = "app"
+urlpatterns = [
+    path('', IndexView.as_view(), name='index')
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+"""Views for the app."""
 
-# Create your views here.
+# from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    """The landing page for the site."""
+
+    template_name = 'app/index.html'
